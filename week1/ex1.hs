164c6964be5dc54638898e97a81bea91e4a3a3a5
Nome do Usuário <email@gmail.com>
2025-09-23 11:04:23 -0300

    init: cria projeto  e adiciona funções básicas e exemplos de uso

---
+++ week1/ex1.hs
@@ -0,0 +1,29 @@
+incr :: Int-> Int 
+incr x = x+1 
+
+triple :: Int-> Int 
+triple x = 3*x 
+
+welcome :: String-> String 
+welcome name = "Hello, " ++ name ++ "!" 
+
+count :: String-> String 
+count str = show (length str) ++ " characters."
+
+-- No terminal, escreve ghci e abre o terminal.
+-- ghci :l ex1.hs (carrega o arquivo que deseja)
+-- Assim que carregar, já pode usar os módulos
+-- Num e => e -> e é uma forma de aceitar números, float, real, etc. Não apenas inteiro, ou double, ou float, por exemplo
+
+-- Executar no terminal:
+-- (a) incr (triple 3) 
+-- (b) triple (incr (3+1)) 
+-- (c) triple (incr 3 + 1) 
+-- (d) triple (incr 3) + 1 
+-- (e) welcome "Harry" ++ welcome "Potter" 
+-- (f) welcome ("Harry" ++ " Potter") 
+-- (g) welcome (welcome "Potter") 
+-- (h) count "Expelliarmus!" 
+-- (i) count (count "Expelliarmus!")
+
+
