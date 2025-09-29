8f071275d854b048acd4107b5d7f918f1b70a9c8
anaalthoff <anapaula.althoff@gmail.com>
2025-09-29 07:29:21 -0300

    feat: adiciona funções com if e pattern match

--- week1/ex8.hs
+++ week1/ex8.hs
@@ -2,6 +2,10 @@
 five :: Int -> String
 five n = if n == 5 then "Five" else "Not Five"
 
+even x = if (x `mod` 2) == 0 
+        then "Even"
+        else "Odd"
+
 five' :: Int -> String
 five' n
   | n == 5 = "Five"
@@ -11,6 +15,14 @@ five' n
 -- Pattern match
 five'' 5 = "five"
 five'' _ = "not five"
+
+not :: Bool -> Bool
+not True = False
+not False = True
+
+not' :: Bool -> Bool
+not' True = False
+not' _ = True
 
 -- 'Case' faz pattern match sobre o que está após o case
 five''' n = case n of
