c49a2d9fe51c9476a5f9bf35361270eb84ea5146
anaalthoff <anapaula.althoff@gmail.com>
2025-11-04 11:58:12 -0300

    refactor: reorganize the triangleArea function e add explanatory comments

--- week1/ex5.hs
+++ week1/ex5.hs
@@ -1,7 +1,13 @@
+-- 1.5 We can compute the area A of a triangle whose sides measure a, b, c using the following formula:
+-- A = s(s −a)(s −b)(s −c), where s = (a+b+c)/2.
+-- Complete the following Haskell function definition to compute this area.
+
 triangleArea :: Float -> Float -> Float -> Float
-triangleArea a b c = sqrt (s*(s-a)*(s-b)*(s-c)) 
-    where s = (a+b+c)
+triangleArea a b c = sqrt (s * (s - a) * (s - b) * (s - c))
+  where
+    s = (a + b + c)
 
-triangleArea' a b c = let s = (a+b+c) / 2 in
-    sqrt (s*(s-a)*(s-b)*(s-c))
-
+triangleArea' :: Float -> Float -> Float -> Float
+triangleArea' a b c =
+  let s = (a + b + c) / 2
+   in sqrt (s * (s - a) * (s - b) * (s - c))
