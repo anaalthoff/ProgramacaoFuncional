0f365530b6d1914ec30bad26da2baae764d0ec18
anaalthoff <anapaula.althoff@gmail.com>
2025-11-10 08:33:11 -0300

    feat: adiciona funções utilizando pattern matching

--- week2/ex4.hs
+++ week2/ex4.hs
@@ -1,1 +1,9 @@
--- 2.4 Write a definition of the exclusive or function xor :: Bool-> Bool-> Bool using multiple equations with patterns.
+-- 2.4 Write a definition of the exclusive or function xor :: Bool -> Bool -> Bool using multiple equations with patterns.
+-- Pattern matching (ou “casamento de padrões”) é um recurso do Haskell (e de outras linguagens funcionais) que permite definir funções de acordo com a forma ou valor dos argumentos.
+-- Ou seja, descreve “casos” específicos para os argumentos, e Haskell escolhe automaticamente qual caso usar.
+
+xor :: Bool -> Bool -> Bool
+xor False False = False
+xor False True  = True
+xor True  False = True
+xor True  True  = False
